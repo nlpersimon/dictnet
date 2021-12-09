6f15593dc8618447e87cac44aabdabdd16c776ee
Simon Lee <simon2020291@gmail.com>
2021-12-09 16:55:16 +0800

    feat: pass a folder path to `load_sensenet()` instead of file paths

--- src/sensenet/sensenet/__init__.py
+++ src/sensenet/sensenet/__init__.py
@@ -1,8 +1,15 @@
+import os
 from .base_sensenet import BaseSenseNet
 from .max_sensenet import MaxSenseNet
 
 
-def load_sensenet(senset_file_path: str, sense_embeds_path: str, sense_embedder_path: str) -> BaseSenseNet:
+def load_sensenet(data_folder_path: str) -> BaseSenseNet:
+    def join_path(file_name):
+        return os.path.join(data_folder_path, file_name)
+
+    senset_file_path = join_path('senset_file.jsonl')
+    sense_embeds_path = join_path('sense_embeddings.txt')
+    sense_embedder_path = join_path('sense_embedder/model.tar.gz')
     sensenet = MaxSenseNet.from_path(
         senset_file_path, sense_embeds_path, sense_embedder_path)
     return sensenet
