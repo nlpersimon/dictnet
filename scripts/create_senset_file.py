4254f7ddc488f3dda5399a487003de757ad5d9a8
Simon Lee <simon2020291@gmail.com>
2021-12-09 17:34:32 +0800

    feat: convert the scripts to cmd tools

--- scripts/create_senset_file.py
+++ scripts/create_senset_file.py
@@ -1,3 +1,4 @@
+import argparse
 import jsonlines
 from gensim.models import KeyedVectors
 from src.sensenet.schema.sense_file import SenseFileReader
@@ -5,23 +6,35 @@ from src.sensenet.schema.senset_file import SensetFileWriter
 from src.sensenet.sas.sensets_extractor import SensetsExtractor
 
 
+def prepare_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sense_file', type=str, dest='sense_file',
+                        help='path to the sense file')
+    parser.add_argument('-e', '--sense_embeds', type=str, dest='sense_embeds',
+                        help='path to the sense embeddings file')
+    parser.add_argument('-o', '--output', type=str, dest='output',
+                        help='path to the output senset file')
+    return parser
+
+
 def main():
+    parser = prepare_parser()
+    args = parser.parse_args()
+
     sense_embeds = KeyedVectors.load_word2vec_format(
-        'src/sensenet/ses/embeddings/wn_bi-camb_cpae.txt',
+        args.sense_embeds,
         binary=False
     )
 
-    with jsonlines.open('data/v0.0.1/sense_file_bi-camb.jsonl') as f:
+    with jsonlines.open(args.sense_file) as f:
         senses = list(SenseFileReader(f).read())
 
     sensets_extractor = SensetsExtractor(senses, sense_embeds)
     all_sensets = sensets_extractor.all_sensets()
-    # all_sensets = sensets_extractor.sensets('apple', 'NOUN')
 
-    with jsonlines.open('data/v0.0.1/senset_file_bi-camb.jsonl', 'w') as f:
+    with jsonlines.open(args.output, 'w') as f:
         writer = SensetFileWriter(f)
         for _, sensets in all_sensets.items():
-            # for senset in all_sensets:
             for senset in sensets:
                 writer.write(senset)
 
